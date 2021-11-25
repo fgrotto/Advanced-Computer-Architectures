4fd7c61f339ea37908f51cf3d11fd17993f95a6d
Filippo <filippo.grotto@gmail.com>
2021-11-25 18:33:34 +0000

    Add image info

--- lesson_4/00_GaussianBlur/GaussianBlur.cu
+++ lesson_4/00_GaussianBlur/GaussianBlur.cu
@@ -5,13 +5,38 @@
 #include "Timer.cuh"
 #include "CheckError.cuh"
 #include <opencv2/opencv.hpp>
+#include <string>
+
 using namespace timer;
 
-const int N = 10; // it has to be divisible by 2
+const int N = 5;
 const int WIDTH  = 1000;
 const int HEIGHT = 500;
 const int CHANNELS = 3;
 const int BLOCK_SIZE = 32;
+
+std::string getImageType(int type) {
+  std::string r;
+
+  uchar depth = type & CV_MAT_DEPTH_MASK;
+  uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+  switch ( depth ) {
+    case CV_8U:  r = "8U"; break;
+    case CV_8S:  r = "8S"; break;
+    case CV_16U: r = "16U"; break;
+    case CV_16S: r = "16S"; break;
+    case CV_32S: r = "32S"; break;
+    case CV_32F: r = "32F"; break;
+    case CV_64F: r = "64F"; break;
+    default:     r = "User"; break;
+  }
+
+  r += "C";
+  r += (chans+'0');
+
+  return r;
+}
 
 template <class T>
 void printImageForDebug(T *image, int N, int width, int channels)
@@ -34,24 +59,34 @@ void printImageForDebug(T *image, int N, int width, int channels)
 	std::cout << std::endl;
 }
 
+void buildGaussianFilterMask1D (float *Mask, int N, float sigma) {
+	float sum = 0.0;
+	
+	for(int j=0; j<N; j++) {
+		Mask[j] = 1 / ( sqrt(2 * M_PI) * sigma ) * exp( -( pow( abs( N / 2 - j ), 2 ) ) / ( 2 * pow( sigma, 2 ) ) );
+		sum += Mask[j];
+	}
+
+	for(int j=0; j<N; j++) {
+		Mask[j] = Mask[j] / sum;
+	}
+}
 
 void buildGaussianFilterMask(float *Mask, int N, float sigma)
 {	
-    double r, s = 2.0 * sigma * sigma;
-    double sum = 0.0;
-
-    for (int x = -N/2; x < N/2; x++) {
-        for (int y = -N/2; y < N/2; y++) {
-            r = sqrt(x * x + y * y);
-            Mask[(x + N/2) * N + (y + N/2)] = (exp(-(r * r) / s)) / (M_PI * s);
-            sum += Mask[(x + N/2) * N + (y + N/2)];
-        }
-    }
- 
-    for (int i = 0; i < N; ++i)
-        for (int j = 0; j < N; ++j)
-            // Mask[i *N + j] /= sum;
-			Mask[i *N + j] = 1.0/100.0; // Fixed smoothing filter
+    float sum = 0.0;
+	
+	for(int i=0; i<N; i++) {
+		for(int j=0; j<N; j++) {
+			Mask[i*N+j] = 1 / ( 2 * M_PI * pow( sigma, 2 ) ) * exp( -( pow( abs( N / 2 - i ), 2 ) + pow( abs( N / 2 - j ), 2 ) ) / ( 2 * pow( sigma, 2 ) ) );
+			sum += Mask[i*N+j];
+		}
+	}
+	for(int i=0; i<N; i++) {
+		for(int j=0; j<N; j++) {
+			Mask[i*N+j] = Mask[i*N+j] / sum;
+		}
+	}
 }
 
 __global__ void GaussianBlurDevice(const unsigned char *image,
@@ -79,40 +114,9 @@ __global__ void GaussianBlurDevice(const unsigned char *image,
 			}
 			image_out[(globalId_y * WIDTH + globalId_x) * CHANNELS + channel] = (unsigned char)pixel_value;
 		}
+		// image_out[(globalId_y * WIDTH + globalId_x) * CHANNELS + 3] = (unsigned char)255;
 	}
 }
-
-// __global__ void GaussianBlurDeviceSharedMemory(const unsigned char *image,
-// 								   const float *mask,
-// 								   unsigned char *image_out,
-// 								   int N)
-// {
-//     __shared__ float sharedMem[5000];
-
-// 	int globalId_x = threadIdx.x + blockIdx.x * blockDim.x;
-// 	int globalId_y = threadIdx.y + blockIdx.y * blockDim.y;
-
-// 	if (globalId_x < WIDTH && globalId_y < HEIGHT)
-// 	{
-// 		for (int channel = 0; channel < CHANNELS; ++channel)
-// 		{
-// 			sharedMem[(threadIdx.y * blockDim.x + threadIdx.x) * CHANNELS + channel] = image[(globalId_y*WIDTH+globalId_x) * CHANNELS + channel];
-// 			__syncthreads();
-
-// 			float pixel_value = 0;
-// 			for (int u = 0; u < N; u++)
-// 			{
-// 				for (int v = 0; v < N; v++)
-// 				{
-// 					int new_x = min(WIDTH, max(0, threadIdx.x + u));
-// 					int new_y = min(HEIGHT, max(0, threadIdx.y + v));
-// 					pixel_value += mask[v * N + u] * sharedMem[(new_y * blockDim.x + new_x) * CHANNELS + channel];
-// 				}
-// 			}
-// 			image_out[(globalId_y * WIDTH + globalId_x) * CHANNELS + channel] = (unsigned char)pixel_value;
-// 		}
-// 	}
-// }
 
 __global__ void GaussianBlurDeviceRow(const unsigned char *image,
 								   const float *mask,
@@ -135,6 +139,7 @@ __global__ void GaussianBlurDeviceRow(const unsigned char *image,
 			}
 			image_out[(globalId_y * WIDTH + globalId_x) * CHANNELS + channel] = (unsigned char)pixel_value;
 		}
+		// image_out[(globalId_y * WIDTH + globalId_x) * CHANNELS + 3] = (unsigned char)255;
 	}
 }
 
@@ -159,6 +164,7 @@ __global__ void GaussianBlurDeviceColumn(const unsigned char *image,
 			}
 			image_out[(globalId_y * WIDTH + globalId_x) * CHANNELS + channel] = (unsigned char)pixel_value;
 		}
+		// image_out[(globalId_y * WIDTH + globalId_x) * CHANNELS + 3] = (unsigned char)255;
 	}
 }
 
@@ -186,6 +192,7 @@ void GaussianBlurHost(const unsigned char *image,
 				}
 				image_out[(y * WIDTH + x) * CHANNELS + channel] = (unsigned char)pixel_value;
 			}
+			// image_out[(y * WIDTH + x) * CHANNELS + 3] = (unsigned char)255;
 		}
 	}
 }
@@ -203,6 +210,9 @@ int main()
 		exit(0);
 	}
 
+	std::string ty =  getImageType( img.type() );
+	printf("Image info: %s %dx%d \n", ty.c_str(), img.cols, img.rows );
+
 	// cv::namedWindow("Display window");
 	// cv::imshow("Display window", img);
 	// cv::waitKey(0);
@@ -214,16 +224,14 @@ int main()
 	unsigned char *device_image_out = new unsigned char[WIDTH * HEIGHT * CHANNELS];
 
 	float *mask = new float[N * N];
-	float sigma = 1.0;
+	float *mask_row = new float[N]; 
+	float sigma = 3.0;
 	image = img.data;
 
 	// Build a gaussian filter for the image
 	buildGaussianFilterMask(mask, N, sigma);
-
-	float mask_row []= { 1.0/10.0, 1.0/10.0, 1.0/10.0,
-						 1.0/10.0, 1.0/10.0, 1.0/10.0,
-						 1.0/10.0, 1.0/10.0, 1.0/10.0, 1.0/10.0 };
-
+	buildGaussianFilterMask1D(mask_row, N, sigma);
+	
 	// -------------------------------------------------------------------------
 	// HOST EXECUTIION
 	TM_host.start();
@@ -265,8 +273,8 @@ int main()
 	TM_device.start();
 
 	GaussianBlurDevice<<<block_size, num_blocks>>>(dev_image, dev_mask, dev_image_out, N);
-	// GaussianBlurDeviceRow<<<block_size, num_blocks>>>(dev_image, dev_mask_row, tmp, N);
-	// GaussianBlurDeviceColumn<<<block_size, num_blocks>>>(tmp, dev_mask_row, dev_image_out, N);
+	// GaussianBlurDeviceColumn<<<block_size, num_blocks>>>(dev_image, dev_mask_row, tmp, N);
+	// GaussianBlurDeviceRow<<<block_size, num_blocks>>>(tmp, dev_mask_row, dev_image_out, N);
 
 	TM_device.stop();
 	CHECK_CUDA_ERROR
@@ -292,7 +300,7 @@ int main()
 	{
 		for (int j = 0; j < WIDTH; j++)
 		{
-			if (device_image_out[i * WIDTH + j] != host_image_out[i * WIDTH + j])
+			if (host_image_out[i*WIDTH+j] < device_image_out[i*WIDTH+j]-1 || host_image_out[i*WIDTH+j] > device_image_out[i*WIDTH+j]+1 )
 			{
 				std::cerr << "wrong result at [" << i << "][" << j << "]!" << std::endl;
 				std::cerr << "image_out: " << (short)host_image_out[i * WIDTH + j] << std::endl;
